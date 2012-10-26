ac75cacf67516799479863aef493b3fddf6c1942
Sam Davies <sam.jc.davies@gmail.com>
2012-10-26 20:20:54 +0100

    Initial Commit

---
+++ Annulus/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Annulus
+//
+//  Created by Sam Davies on 26/10/2012.
+//  Copyright (c) 2012 VisualPutty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Annulus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Annulus
+//
+//  Created by Sam Davies on 26/10/2012.
+//  Copyright (c) 2012 VisualPutty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
